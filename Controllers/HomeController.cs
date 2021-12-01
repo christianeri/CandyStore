67b4b94fe7d66d2d0215b152a3990acb65db63a9
Christian Eriksson <e.christian@live.se>
2021-12-01 08:16:21 +0100

    Before Part 6

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using CandyStore.Models;
+using CandyStore.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CandyStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ICandyRepository candyRepository;
+        private readonly ICategoryRepository categoryRepository;
+
+        public HomeController(ICandyRepository candyRepository)
+        {
+            this.candyRepository = candyRepository;
+        }
+
+
+
+
+        public IActionResult Index()
+        {
+            var homeVievModel = new HomeViewModel
+            {
+                CandyOnSale = candyRepository.GetCandyOnSale
+            };
+            return View(homeVievModel);
+        }
+    }
+}
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CandyStore.Models;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CandyStore.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Candy> CandyOnSale  { get; set; }
+    }
+}
